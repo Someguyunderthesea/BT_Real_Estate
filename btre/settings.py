b0e595de6f376add2604c5fbf2da78468fd17e7d
Andrew Pan <andrewcypan@gmail.com>
2025-03-19 10:56:13 -0400

    SUPA

--- btre/settings.py
+++ btre/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+
 from supabase import create_client
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
